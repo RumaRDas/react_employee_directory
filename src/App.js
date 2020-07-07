f73049da3c6eeb55afec9784f66e884725512382
Ruma Das <rumaadl16@gmail.com>
2020-07-08 00:29:26 +0930

    adding api in project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/header/Header';
 import Search from './components/Search';
 import Employees from "./components/Employees";
@@ -6,31 +6,35 @@ import Axios from 'axios';
 
 import './App.css';
 
- function App() {
-//   const [search, setSearch] = useState("");
-//   const [employees, setEmployees] = useState([]);
-  
-//   useEffect(() => {
-//    getEmployees();
-//   }, [])
+function App() {
+  const [search, setSearch] = useState("name");
+  const [employees, setEmployees] = useState([]);
 
-// const getEmployees = async () => {
-//   const result = await Axios.get();
-//   setEmployees(result.data.hits);
-// };
-// const onInputchange =(e) =>{
-//   setSearch(e.target.value);
-// }
-// const searchClick = () =>{
-//   getEmployees();
-// }
+  useEffect(() => {
+ getEmployees();
+  }, [])
 
-   return (
+  const getEmployees = async () => {
+    const res = await Axios.get(`https://randomuser.me/api/?results=20`);
+    //console.log(res.data);
+    setEmployees(res.data.results);
+  };
+
+  //for search option
+  const onInputchange = (e) => {
+    console.log(e.target.value);
+    setSearch(e.target.value);
+  }
+  // const searchClick = () =>{
+  //   getEmployees();
+  // }
+
+  return (
     <div className="App">
 
-<Header />
-<Search/>
-<Employees />
+      <Header />
+      <Search search={search} onInputchange={onInputchange} />
+      <Employees employees={employees} />
 
     </div>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,16 @@
 import React from 'react'
 
 
-const Search =() => {
+const Search =(props) => {
 
         return (
             <div className="search-back">
             <div className="input-group w-50 mx-auto">
-            <input type="text" className="form-control" placeholder=""  />
+            <input type="text" 
+            className="form-control"
+             placeholder="Search Employee by name..."
+             value={props.search}  
+             onChange={props.onInputchange}/>
             <div className="input-group-append">
               <button className="btn btn-dark">Search</button>
             </div>
