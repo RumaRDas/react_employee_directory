d63859865826d8f68ab3c9332bd6ca348717dc7f
Ruma Das <rumaadl16@gmail.com>
2020-07-07 19:43:51 +0930

    Adding container and alert file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Header from './components/header/Header';
 import Search from './components/header/Search';
+
 import './App.css';
 
 function App() {
    return (
     <div className="App">
+
 <Header />
 <Search/>
+
     </div>
   );
 }
